00aa97a31736612f812fef9f7fec8cb7b0c6f24c
Anders Ryberg <anders@razlogic.se>
2026-02-08 17:11:12 +0100

    macos 0.2.0

--- flutter/razmanager/lib/public/event/public_event.model.dart
+++ flutter/razmanager/lib/public/event/public_event.model.dart
@@ -277,10 +277,9 @@ class EventModel extends ChangeNotifier with GrpcClient {
         .listen(
           (data) {
             _audioPlayer ??= AudioPlayer();
-
             // https://github.com/bluefireteam/audioplayers/issues/1269
-            _audioPlayer!.play(UrlSource(Uri.dataFromBytes(Uint8List.fromList(data.speech.value), mimeType: "audio/mpeg").toString()));
-            //_audioPlayer!.play(BytesSource(Uint8List.fromList(data.speech.value)));
+            //_audioPlayer!.play(UrlSource(Uri.dataFromBytes(Uint8List.fromList(data.speech.value), mimeType: "audio/mpeg").toString()));
+            _audioPlayer!.play(BytesSource(Uint8List.fromList(data.speech.value)));
           },
           onDone: () => debugPrint('_eventSpeechSubscribe done'),
           onError: (exception) async {
